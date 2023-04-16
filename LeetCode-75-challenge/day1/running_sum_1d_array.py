e20fdd3f9f1679249016c2f209ce87a7ef110113
AshrafOmara12 <ashrafomara33@gmail.com>
2023-04-17 01:21:15 +0200

    leetcode problems solving

---
+++ LeetCode-75-challenge/day1/running_sum_1d_array.py
@@ -0,0 +1,16 @@
+# problem link: https://leetcode.com/problems/running-sum-of-1d-array/?envType=study-plan&id=level-1
+
+# answer
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        sums = []
+        for i in range(len(nums)):
+            if len(sums) == 0:
+                sums.append(nums[i])
+            else:
+                current = nums[i]
+                previous = sums[i-1]
+                print(current, previous)
+                total = current + previous
+                sums.append(total) 
+        return sums
